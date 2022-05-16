b40eeb6b266478d8038d30acd220dec6357109c5
KamaleshKannan <81850601+KamaleshKannan@users.noreply.github.com>
2022-05-16 12:26:10 +0530

    Delete Admin-Service directory

--- Admin-Service/src/main/java/kam/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package kam.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import kam.models.Admin;
-
-public interface AdminRepository extends MongoRepository<Admin, String> {
-
-}
