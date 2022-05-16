6fef2a202498805e294a9349e4c3f199fb450dfc
KamaleshKannan <81850601+KamaleshKannan@users.noreply.github.com>
2022-05-16 12:32:11 +0530

    Add files via upload

---
+++ EurekaServerMicroservice/src/test/java/com/discovery/server/systemdiscovery/SystemDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery.server.systemdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
